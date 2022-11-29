d95d0e758f274c829f2a642d6c09859c114198c5
Jacob <jacobschwal@gmail.com>
2022-11-28 20:50:56 -0600

    adding semantic-ui-css

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
