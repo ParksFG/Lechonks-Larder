7e8eb9df3d28000b18b24680bae0e41345555a40
Tee Oni <tzepeda4@gmail.com>
2022-11-28 19:39:04 -0600

    Added additional queries to schema.js for GraphQL

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,11 +1,11 @@
 const { cards, users } = require('../sampleData')
 
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema,} = require('graphql');
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList, buildClientSchema,} = require('graphql');
 
 
 // Card Type
 const CardType = new GraphQLObjectType({
-    name: 'cards',
+    name: 'card',
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
@@ -18,25 +18,97 @@ const CardType = new GraphQLObjectType({
 });
 
 const UserType = new GraphQLObjectType({
-    name: 'users',
+    name: 'user',
     fields: () => ({
         id: { type: GraphQLID },
-        uid: {}
+        uid: { type: GraphQLString },
+        username: { type: GraphQLString },
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
     })
-})
+});
 
 
 //Root Query
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields:{
+
+        //Allows us to query for all cards
         cards: {
+            type: new GraphQLList(CardType),
+            resolve(parent, args) {
+                return cards;
+            },
+        },        
+        
+        //Allows us to query for a specific card based on card id
+        card: {
             type: CardType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 return cards.find(cards => cards.id === args.id);
             }
-        }
+        },
+
+        //Allows us to query for a specific card based on card Name
+        cardname:  {
+            type: CardType,
+            args: { name: { type: GraphQLID } },
+            resolve(parent, args) {
+                return cards.find(cards => cards.name === args.name);
+            }
+        },
+
+        //Allows us to query for a cards by UID[user id]
+        uidscard: {
+            type: new GraphQLList(CardType),
+            args: { uid: { type: GraphQLString } },
+            resolve(parent, args) {
+                return cards.filter(cards => cards.uid === args.uid);
+            }
+        },
+
+        //Allows us to query for cards by username
+        usernamescard: {
+            type: new GraphQLList(CardType),
+            args: { username: { type: GraphQLString } },
+            resolve(parent, args) {
+                return cards.filter(cards => cards.username === args.username);
+            }
+        },
+
+        //Allows us to query for users
+        users: {
+            type: new GraphQLList(UserType),
+            args: { username: { type: GraphQLString } },
+            resolve(parent, args) {
+                return users;
+            }
+        },
+
+        //Allows us to query for an individual user by username
+        user: {
+            type: UserType,
+            args: { username: { type: GraphQLString } },
+            resolve(parent, args) {
+                return users.find( users => users.username === args.username);
+            }
+        },
+
+        //Allows us to query for an individual user by email
+        useremail: {
+            type: UserType,
+            args: { email: { type: GraphQLString } },
+            resolve(parent, args) {
+                return users.find( users => users.email === args.email);
+            }
+        },
+
+
+        
+
+
     }
 });
 
