f63300485cfb4e7b27cf476765f687ed1390e1fa
Jacob <jacobschwal@gmail.com>
2022-11-29 19:38:17 -0600

    added code to index and app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import homePage from "./landing/pages/homePage";
+import loginPage from "./landing/pages/loginPage"
+
+function App() {
+    return (
+        <homePage />
+    );
+}
+
+export default App;
