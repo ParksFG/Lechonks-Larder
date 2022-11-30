cd74a1b1520288cb3bf5ebb47548d12f0ddb096e
Jacob <jacobschwal@gmail.com>
2022-11-29 19:13:59 -0600

    added starter script to app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import homePage from "./landing/pages/homePage";
+
+function App() {
+    return (
+        <homePage />
+    );
+}
+
+export default App;
