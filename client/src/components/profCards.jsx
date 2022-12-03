e7cae0ecb1568d2bf8f64986dab8ba62cc6b82bf
Charles Glasgow <brevenjh@gmail.com>
2022-12-03 17:10:39 -0600

    reformatted pages and added components folder for profile tabs

---
+++ client/src/components/profCards.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Card, Icon, Image } from 'semantic-ui-react'
+
+const ProfCards = () => (
+  <Card>
+    <Image src='https://images.pokemontcg.io/swsh9/18_hires.png' wrapped ui={false} />
+  </Card>
+)
+
+export default ProfCards;
