117410c967e7994598c12a843dbd902b5325be4f
sky19930112 <107167603+sky19930112@users.noreply.github.com>
2022-12-05 17:02:46 -0600

    update user model

--- server/models/user.js
+++ server/models/user.js
@@ -5,33 +5,30 @@ const cardSchema = require("./card");
 
 const userSchema = new Schema(
   {
+    uid: {
+      type: String
+    },
     username: {
-      type: String,
-      required: true,
-      unique: true,
+      type: String
     },
     email: {
       type: String,
-      required: true,
-      unique: true,
-      match: [/.+@.+\..+/, "valid email address"],
+      match: [/.+@.+\..+/, "valid email address"]
     },
     password: {
       type: String,
       required: true,
     },
     // set cards to be an array of data to the cardSchema
-    savedCards: [cardSchema],
-  },
-  // set this to use virtual below
-  {
-    toJSON: {
-      virtuals: true,
-    },
+    savedCards: [cardSchema]
   }
 );
+module.exports = mongoose.model('user', userSchema);
+
+
 
 // set up pre-save middleware to create password
+/*
 userSchema.pre("save", async function (next) {
   if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
@@ -49,3 +46,5 @@ userSchema.methods.isCorrectPassword = async function (password) {
 const User = model("User", userSchema);
 
 module.exports = User;
+
+*/
