0d65a1de1ff8d0fc225b454d60f8872fbc2478bd
Jacob <jacobschwal@gmail.com>
2022-12-06 16:50:46 -0600

    added new styling to the homepage and added navbar to header page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,35 +1,47 @@
 import React from "react";
-import { Segment, Container, Grid, Image } from "semantic-ui-react";
+import { Segment, Container, Grid, Image, Header } from "semantic-ui-react";
 
 const Home = () => {
-
-    return (
-            
-        <Segment>
-            <Container >
-            <h2>Pokemon Cards of the Day</h2>
-            </Container>,
-            <Container>
-                <Grid columns={3}>
-                    <Grid.Row>
-                        <Grid.Column>
-                            <Image src='https://images.pokemontcg.io/swsh9/174.png' />
-                        </Grid.Column>
-                        <Grid.Column>
-                             <Image src='https://images.pokemontcg.io/swshp/SWSH262.png' />
-                        </Grid.Column>
-                        <Grid.Column>
-                        <Image src='https://images.pokemontcg.io/swsh9/18.png' />
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </Container>
+  return (
+    <div className="homepageDiv">
+      <div className="homepageDiv" style={{ backgroundcolor: "Black" }}>
+        <Segment textAlign="center" style={{ padding: "10em 0em" }} vertical>
+          <Container text inverted>
+            <Header
+              classname="homepageHeader"
+              style={{ fontSize: "2em", color: "White" }}
+            >
+              Welcome to LeChonks Larder!
+            </Header>
+            <p style={{ fontSize: "1.33em" }}>
+              Welcome to the number one Pokemon card collector site! Here you
+              can find and collect all the cards you have in your collection!
+            </p>
+          </Container>
         </Segment>
+      </div>
+      <Segment>
+        <Container>
+          <h2>Pokemon Cards of the Day</h2>
+        </Container>
+        <Container>
+          <Grid columns={3}>
+            <Grid.Row>
+              <Grid.Column>
+                <Image src="https://images.pokemontcg.io/swsh9/174.png" />
+              </Grid.Column>
+              <Grid.Column>
+                <Image src="https://images.pokemontcg.io/swshp/SWSH262.png" />
+              </Grid.Column>
+              <Grid.Column>
+                <Image src="https://images.pokemontcg.io/swsh9/18.png" />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </Segment>
+    </div>
+  );
+};
 
-
-
-    )
-
-}
-
-export default Home
+export default Home;
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,40 +1,43 @@
-import React from 'react'
-import { List, Segment } from 'semantic-ui-react'
+import React from "react";
+import { List, Segment } from "semantic-ui-react";
 
 const NavBar = () => {
-return (
-  
-  <Segment textAlign='center'>
+  return (
+    <div>
+    <Segment textAlign="center">
+      <List horizontal size="massive">
+        <List.Item>
+          <List.Content>
+            <List.Header a href="/" color="white">
+              Home
+            </List.Header>
+          </List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Content>
+            <List.Header a href="/Search">
+              Search
+            </List.Header>
+          </List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Content>
+            <List.Header a href="/Login">
+              Login
+            </List.Header>
+          </List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Content>
+            <List.Header a href="/Profile">
+              Profile
+            </List.Header>
+          </List.Content>
+        </List.Item>
+      </List>
+    </Segment>
+  </div>
+  );
+};
 
-  
-  <List horizontal size='massive'>
-    <List.Item>
-        <List.Content>
-        <List.Header a href="/" color="white">Home</List.Header>
-        </List.Content>
-    </List.Item>
-    <List.Item>
-        <List.Content>
-        <List.Header a href='/Search'>Search</List.Header>
-        </List.Content>
-    </List.Item>
-    <List.Item>
-        <List.Content>
-        <List.Header a href='/Login'>Login</List.Header>
-        </List.Content>
-    </List.Item>
-    <List.Item>
-        <List.Content>
-        <List.Header a href='/Profile'>Profile</List.Header>
-        </List.Content>
-    </List.Item>
-  </List>
-
-
-  </Segment>
-)
-
-}
-
-
-export default NavBar
+export default NavBar;
