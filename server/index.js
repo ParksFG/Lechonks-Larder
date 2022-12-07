306e53a68135d6bd0b66c4557e8576046652dab5
ParksFG <ParkerFGay@gmail.com>
2022-12-06 18:34:33 -0600

    Various fixes for heroku deployment

--- server/index.js
+++ server/index.js
@@ -6,14 +6,26 @@ const schema = require('./schema/schema.js');
 const app = express();
 const connectDB = require('./config/db');
 const cors = require('cors');
+const path = require('path');
 
 connectDB();
 
 app.use(cors());
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true,
 }));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.listen(port, console.log(`Server Running on port ${port}`));
